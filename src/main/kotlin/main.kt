fb2da8724e7aff6140a91b0d8725a748f7f2d05e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-09-23 14:25:24 -0700

    Output audio files instead of commands.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,12 +5,29 @@ fun main(args: Array<String>) {
     val audioFile = File(args[0])
     val silenceFile = File(args[1])
     val sentences = FindSentences.from(silenceFile)
-    val commands = sentences.mapIndexed { index, sentence ->
-        val sentenceFilename = String.format("sentence%03d", index)
-        val printDuration = sentence.duration.toPrint()
-        "ffmpeg -i ${audioFile.canonicalPath} -ss ${sentence.start} -to ${sentence.end} -c copy $sentenceFilename.mp3 # $printDuration"
+    val outDir = File("splits").apply {
+        deleteRecursively()
+        mkdirs()
     }
-    commands.forEach(::println)
+    val commands = sentences.mapIndexed { index, sentence ->
+        val sentenceFilename = String.format("sentence%03d.mp3", index)
+        val outFileSpec = File(outDir, sentenceFilename)
+        arrayOf(
+            "ffmpeg",
+            "-i", audioFile.canonicalPath,
+            "-metadata", "title=\"Sentence $index\"",
+            "-ss", "${sentence.start}",
+            "-to", " ${sentence.end}",
+            "-c", "copy", outFileSpec.path
+        )
+    }
+    commands.forEach { commandArray ->
+        println(commandArray)
+        val process = Runtime.getRuntime().exec(commandArray)
+        process.errorStream.use {
+            println(it.reader().readText())
+        }
+    }
     println("Min Duration: ${sentences.map { it.duration }.min().toPrint()}")
     println("Max Duration: ${sentences.map { it.duration }.max().toPrint()}")
     println("Count: ${sentences.size}")
