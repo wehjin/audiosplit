7c94260754b043b1b14b0bc027fb194b716aa25c
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-09-26 18:50:48 -0700

    Add action parameter.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,19 +2,31 @@ import java.io.File
 
 
 fun main(args: Array<String>) {
-    val audioFile = File(args[0])
-    val silenceFile = File(args[1])
+    val action = args[0]
+    val audioFile = File(args[1])
+    val silenceFile = File(args[2])
     val sentences = FindSentences.from(silenceFile)
-    val outDir = File("splits").apply {
-        deleteRecursively()
-        mkdirs()
+    when (action) {
+        "extract" -> {
+            val outDir = File("splits").apply {
+                deleteRecursively()
+                mkdirs()
+            }
+            extractClips(sentences, audioFile, outDir)
+        }
     }
+    println("Min Duration: ${sentences.map { it.duration }.min().toPrint()}")
+    println("Max Duration: ${sentences.map { it.duration }.max().toPrint()}")
+    println("Count: ${sentences.size}")
+}
+
+private fun extractClips(sentences: List<Sentence>, mediaFile: File, clipDir: File) {
     val commands = sentences.mapIndexed { index, sentence ->
         val sentenceFilename = String.format("sentence%03d.mp3", index)
-        val outFileSpec = File(outDir, sentenceFilename)
+        val outFileSpec = File(clipDir, sentenceFilename)
         arrayOf(
             "ffmpeg",
-            "-i", audioFile.canonicalPath,
+            "-i", mediaFile.canonicalPath,
             "-metadata", "title=\"Sentence $index\"",
             "-ss", "${sentence.start}",
             "-to", " ${sentence.end}",
@@ -28,9 +40,6 @@ fun main(args: Array<String>) {
             println(it.reader().readText())
         }
     }
-    println("Min Duration: ${sentences.map { it.duration }.min().toPrint()}")
-    println("Max Duration: ${sentences.map { it.duration }.max().toPrint()}")
-    println("Count: ${sentences.size}")
 }
 
 private fun Double?.toPrint(): String = String.format("%.04fs", this)
